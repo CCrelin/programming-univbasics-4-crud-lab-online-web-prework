d4b86a94fe074829e67acdb019bda494e415ccb1
CLIFF CRELIN <cliff.crelin@gmail.com>
2019-09-10 02:32:25 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -13,9 +13,9 @@ end
 def add_element_to_start_of_array(array, element)
   array.unshift("wow")
 end
-arrays! = array.pop
+
 def remove_element_from_end_of_array(array)
-  
+  array.pop
 end
 
 def remove_element_from_start_of_array(array)
